1d05ac017e945fc0fd162f0950aaaf94c2375c11
OZAWA Sakuro <sakuro@2238.club>
2019-10-14 18:27:45 +0900

    Add day of week lasbels

--- lib/zcal/output.zsh
+++ lib/zcal/output.zsh
@@ -27,16 +27,37 @@ function zcal::print-l() {
   done
 }
 
+function array::rotate-left() {
+  local offset=$1
+  shift
+  local values=("$@")
+  local result=( "${values[@]:$offset:$(($#values - offset))}" "${values[@]:0:$offset}" )
+  echo "${result[@]}"
+}
+
+function zcal::day-of-week-labels() {
+  local start=$1
+  local -A day_of_week_labels=(1 Mo 2 Tu 3 We 4 Th 5 Fr 6 Sa 7 Su)
+  local -a colored_labels=()
+  for w in "${(nk)day_of_week_labels[@]}"; do
+    colored_labels+=( "$(print -nP "%F{$(zcal::date-color $w)}${day_of_week_labels[$w]}%f")" )
+  done
+  local -a result=( $(array::rotate-left $((start - 1)) ${colored_labels[@]}) )
+
+  print "${(j: :)result[@]}"
+}
+
 function zcal::print-x() {
   local -a info=(${(s.:.)1})
-  local head=${info[1]%-*}
+  local year_month=${info[1]%-*}
   local indent=$(( $info[2] - 1 ))
   while [[ $indent -gt 0 ]]; do
     set -- "::" "$@"
     indent=$((indent - 1))
   done
 
-  print $head
+  print "${(l:10:r:10:)year_month}"
+  zcal::day-of-week-labels 1
   while [[ $# -gt 0 ]]; do
     local -a fields=(${(s.:.)1})
     local color="$(zcal::date-color "${fields[2]}" "${fields[3]}")"
